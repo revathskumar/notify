16df29d24551e59a8d258afb8d326608d809cffe
Revath S Kumar <rsk@revathskumar.com>
2015-10-02 04:19:10 +0530

    basic one for testing

--- notify.js
+++ notify.js
@@ -1,29 +1,62 @@
-var Notify = function(){
-  var deskNotif, permission = Notification.permission;
+(function(root, factory){
+  if(typeof exports === 'object') {
+    console.log('this is node');
+    module.exports = factory();
+  }
+  else {
+    console.log('in else since its browser');
+    root.Notify = factory();
+  }
 
-  var send = function(msg) {
-    if(!permission) {
-      return;
-    }
-    if(typeof msg.icon === 'undefined') {
-      msg.icon = "/images/parrot-logo.png";
-    }
-    deskNotif = new Notification(msg.title, msg);
-    deskNotif.onclick = onClick;
-  };
-
-  var onClick = function () {
-    window.focus();
-  };
-
-  var request = function() {
-    console.log('DeskNotifier request');
-    if(permission !== 'denied') {
-      Notification.requestPermission(function(perm){
-        permission = (perm == 'granted');
-      });
+}(this, function(){
+  var Notify = function(opts) {
+    this.options = opts;
+    this.permission = Notification.permission;
+    this.title = opts.title;
+    
+    if(this.permission !== 'granted') {
+      Notification.requestPermission();
     }
   };
+  
+  Notify.prototype.show = function(options) {
+    console.log(options);
+    var n = new Notification(this.options.title, this.options);
+    Notification.onclick = this.options.onClick;
+    Notification.onerror = this.options.onError;
+    n.onshow = this.options.onShow;
+  };
 
-};
+  return Notify;
+
+}));
+
+// var Notify = function(){
+//   var deskNotif, permission = Notification.permission;
+
+//   var send = function(msg) {
+//     if(!permission) {
+//       return;
+//     }
+//     if(typeof msg.icon === 'undefined') {
+//       msg.icon = "/images/parrot-logo.png";
+//     }
+//     deskNotif = new Notification(msg.title, msg);
+//     deskNotif.onclick = onClick;
+//   };
+
+//   var onClick = function () {
+//     window.focus();
+//   };
+
+//   var request = function() {
+//     console.log('DeskNotifier request');
+//     if(permission !== 'denied') {
+//       Notification.requestPermission(function(perm){
+//         permission = (perm == 'granted');
+//       });
+//     }
+//   };
+
+// };
 
---
+++ test.js
@@ -0,0 +1,7 @@
+var Notify = require('./notify.js');
+
+describe('Notify', function(){
+  it('creates an instance', function(){
+    var n = new Notify({title: "Hello"});
+  });
+});
