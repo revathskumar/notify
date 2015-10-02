0d3f5de426437e4481d70040354892ecd2b65112
Revath S Kumar <rsk@revathskumar.com>
2015-10-02 05:31:03 +0530

    saucelab config

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,27 @@
 module.exports = function(config){
+  var customlaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    }
+  };
+
   config.set({
-    basePath: "",
+    basepath: '',
+    sauceLabs: {
+      testName: "Notify"
+    },
     frameworks: ['mocha'],
+    captureTimeout: 120000,
+    customLaunchers: customlaunchers,
+    browsers: Object.keys(customlaunchers),
     files: [
       'notify.js',
       'test.js',                      
-    ]
+    ],
+    reporters: ['saucelabs'],
+    singleRun: true
   })
 }
